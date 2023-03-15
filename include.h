58687f667d104be234affc82b168c49bd1970b56
Xbog <2498462167@qq.com>
2023-03-15 17:23:58 +0800

    diyi

---
+++ include.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <QSerialPort>
+#include <QtDebug>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QTextCodec>
+#include <QTimer>
+#include <comlilerport.h>
+#include <QMessageBox>
+#include "serial.h"
+
+#include <QPainter>
+#include <QBitmap>
+#include <QBrush>
+
+#include "ui_serial.h"
+#endif // INCLUDE_H
